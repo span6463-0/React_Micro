0a014d0bc24874a0c4e88cd63d88be0b59e3f52f
span6463-0 <span6463@sysco.com>
2026-06-14 23:39:49 +0530

    create env files

--- API/item-api/src/index.js
+++ API/item-api/src/index.js
@@ -15,6 +15,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host: process.env.ITEM_DB_HOST || 'localhost',
+    port: Number(process.env.ITEM_DB_PORT) || 5435,
     database: process.env.ITEM_DB_NAME || 'item_db',
     user: process.env.ITEM_DB_USER || 'postgres',
     password: process.env.ITEM_DB_PASSWORD || 'postgres',
--- API/user-api/src/index.js
+++ API/user-api/src/index.js
@@ -14,6 +14,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host: process.env.USER_DB_HOST || 'localhost',
+    port: Number(process.env.USER_DB_PORT) || 5434,
     database: process.env.USER_DB_NAME || 'user_db',
     user: process.env.USER_DB_USER || 'postgres',
     password: process.env.USER_DB_PASSWORD || 'postgres',
